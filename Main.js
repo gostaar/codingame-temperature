257122c0f33eaad061e0f8b665bb68c9a9b1915a
gostaar <99218705+gostaar@users.noreply.github.com>
2022-05-24 14:16:49 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,26 @@
-const n = parseInt().readline();
+const n = parseInt().readLine();
 const inputs = readline();
 
 For (i = 0; i < n; i++) {
- const t = inputs[i]
+ const t = inputs[i]; //chaque valeur
  
- t *= -1; //valeurs positives
+ If (t > 0) {
+  //t *= -1 
+
+ t.sort(function(a, b) {
+   return a-b;
+ }
+
+} else {
+ 
+}
+ 
+ t.sort(function(a, b) {
+   return a-b;
+ }); //Tri variable t
+
+ console.log(inputs[0]); 
+
+
+
 }
